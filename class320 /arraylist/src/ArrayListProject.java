e7c9127d814a0c9b425f79534a23cfd466edf29e
Jjean11 <jljean@protonmail.com>
2024-05-26 23:18:43 -0500

    Add files via upload

---
+++ class320 /arraylist/src/ArrayListProject.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ArrayListProject {
+    public static void main(String[] args) {
+        ArrayList BruceWayne = new ArrayList();
+        BruceWayne.add("Bruce");
+        BruceWayne.add("Wayne");
+        BruceWayne.add("224 Park Drive");
+        BruceWayne.add("Gotham City");
+        BruceWayne.add("71827");
+
+        System.out.println(BruceWayne);
+    }
+}
